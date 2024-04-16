64dfe6e0a9f4f3bd7f380e796cca79d067075f71
AntonioM788 <amerrod832@g.educaand.es>
2024-04-16 20:06:04 +0200

    He añadido cambios en la documentación de las clases java

--- Codigo/biblioteca/src/main/java/com/endes/biblioteca/Autor.java
+++ Codigo/biblioteca/src/main/java/com/endes/biblioteca/Autor.java
@@ -1,21 +1,53 @@
 package com.endes.biblioteca;
+
+import java.util.List;
+
 /**
+ * Representa a un autor de libros dentro de un sistema de gestión de biblioteca.
+ * Contiene información personal del autor y una lista de libros que el autor ha escrito. 
+ * 
  * @author Antonio 
  * @version 1.0 
  */
+
 public class Autor {
     private String name;
     private String biography;
+    private List<Libro> libros;
+    
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Esto es para acceder al nombre del autor 
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	
+	public List<Libro> getLibros() {
+		return libros;
+	}
+	
+	/**
+	 * Esto es para acceder a la Lista de libros 
+	 * @param libros
+	 */
+	public void setLibros(List<Libro> libros) {
+		this.libros = libros;
+	}
+	
 	public String getBiography() {
 		return biography;
 	}
+	
+	/**
+	 * Esto es para acceder a la biografía del autor 
+	 * @param biography
+	 */
 	public void setBiography(String biography) {
 		this.biography = biography;
 	}
